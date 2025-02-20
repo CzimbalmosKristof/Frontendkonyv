33fd8f3cfaceb328cc128a71cd4208e59d30b483
karcsimucsi <mucsikarcsi2003@gmail.com>
2025-02-20 19:11:36 +0100

    regisztráció, bejelentkezés, footer, háttér

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="flex flex-col-reverse">
+    <main className="flex-col" />
+    <footer className="bg-gray-800 text-white py-4 justify-center fixed w-full bottom-0">
+        <div className="container mx-auto text-center">
+            <p>&copy; 2025 Vizsgaremek</p>
+            <ul>
+                <li>Contact: pallagiroland01@gmail.com, mucsikarcsi2003@gmail.com, czimbalmoskristof@gmail.com</li>
+            </ul>
+        </div>
+    </footer></div>
+  )
+}
+
+export default Footer
