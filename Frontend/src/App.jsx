870fd45e08fb41cd82bec4be810a836a21c14358
CzimbalmosKristof <czimbalmos.kristof@gmail.com>
2025-03-06 20:51:46 +0100

    Backend elkezdve

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './components/Login.jsx'
 import Library from './components/Library.jsx'
 import Footer from './components/Footer.jsx'
 import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom';
+
 function App() {
     return (
         <div className='bg-gray-950'>
